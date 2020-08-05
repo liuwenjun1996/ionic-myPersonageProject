5a9c48ffd95f1f710c3e510cc8c88169995eda11
liuwenjun1996 <2469821162@qq.com>
2020-08-05 19:41:34 +0800

    add solution code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  homeList: any = [];
   constructor(public navCtrl: NavController) {
 
   }
+
+  ionViewWillEnter() {
+    this.homeList = [{ value: '1' }, { value: 2 }, { value: 3 }, { value: 4 }, { value: 5 }, { value: 6 }];
+  }
+  ionViewDidEnter() {
+
+  }
+  ionViewWillLoad() {
+  }
+
+  ionViewDidLoad() {
+
+  }
+
 
 }
